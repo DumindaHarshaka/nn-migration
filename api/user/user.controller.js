bf8eb311cd2be1bfff61484b08bf5eb6b1217049
DumindaHarshaka <dumy.gm1@gmail.com>
2016-12-25 11:27:22 +0530

    quick guid to NN

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -55,6 +55,17 @@ module.exports = {
       user.verified = true;
       user.save().then(user => {
         res.redirect("/email-verified");
+        //donation-guide email
+        email.send('donation-guide', {}, {
+          from: config.email_from,
+          to: user.email,
+          subject: 'Quick guide'
+        }).then(function(body) {
+          console.log('Quick guide mail sent');
+        }).catch(function(err) {
+          console.log('Quick guide mail failed');
+          console.log(err);
+        })
       }).then(null, err => {
         next(err)
       })
