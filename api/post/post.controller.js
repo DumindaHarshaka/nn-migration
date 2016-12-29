1680791aa9975f1ce3a4052ea8a291f57fa5ab55
DumindaHarshaka <dumy.gm1@gmail.com>
2016-12-29 21:13:40 +0530

    backend meta data extraction

--- api/post/post.controller.js
+++ api/post/post.controller.js
@@ -3,7 +3,7 @@
 var User = require('../user/user.model');
 var config = require('../../config/environment');
 var email = require('../../email/email.service');
-
+var gm = require('gm');
 
 function validationError(res, statusCode) {
   statusCode = statusCode || 422;
@@ -18,9 +18,37 @@ function handleError(res, statusCode) {
     res.status(statusCode).send(err);
   };
 }
+
+function getMeta(path) {
+  return new Promise(function(resolve, reject) {
+    gm('./' + path).size(function(err, size) {
+      if (!err) {
+        console.log(size.height);
+        resolve({
+          url: path,
+          meta: {
+            height: size.height,
+            width: size.width
+          }
+        });
+      } else {
+        reject(err);
+      }
+    })
+  });
+}
+
 module.exports = {
-  create: function(req,res) {
-    //console.log(req.files);
-    console.log(req.files);
+  create: function(req, res) {
+    var promises = [];
+    for (var i = 0; i < req.files.length; i++) {
+      promises.push(getMeta(req.files[i].path))
+    }
+    Promise.all(promises).then(function(result) {
+      console.log(result);
+    }).catch(function(err) {
+      console.log(err);
+    })
+
   }
 }
